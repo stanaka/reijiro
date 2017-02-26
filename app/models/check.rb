55b2e7fa7ebac3571b59a6951289bcf342558557
Shinji Tanaka <stanaka@mercari.com>
2017-02-26 18:36:10 +0000

    optimize a query for daily stats

--- app/models/check.rb
+++ app/models/check.rb
@@ -6,8 +6,16 @@ class Check < ActiveRecord::Base
 
   class << self
     def check_months
-      checks = Check.find(:all, :order => 'created_at')
-      checks.group_by {|t| t.created_at.to_date}
+      checks = Check.find_by_sql('select date, sum(new) as new_count, count(date) as all_count
+      from (
+        select
+          date(created_at) as date,
+          case when oldstat=0 and newstat=1 then 1 else 0 end as new
+        from checks
+        order by created_at
+      )
+      group by date;
+      ')
     end
   end
 end
